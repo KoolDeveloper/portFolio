665532f35c2202ee2c4d5b13da722c26b90cbc7e
Koolxdev <koolxdev@gmail.com>
2024-05-21 18:47:03 -0500

    Testeando soluciones para el despliegue con github pages

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,17 +1,11 @@
-import { DotGothic16 } from "next/font/google";
 import About from "../ui/About";
 import Skills from "../ui/Skills";
 import Services from "../ui/Services";
 
-const dotGothic16 = DotGothic16({
-  weight: ["400"],
-  style: ["normal"],
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <main className={`${dotGothic16.className}`}>
+    <main>
       <About />
       <section className="w-full grid gap-8 md:gap-10 md:grid-cols-6 md:grid-rows-6 p-6">
         <Skills />
