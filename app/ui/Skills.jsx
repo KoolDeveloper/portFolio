6a7d3e23857885657ef1866e4a4ea09e5721edbb
Koolxdev <koolxdev@gmail.com>
2024-05-21 19:12:33 -0500

    Testeando soluciones para el despliegue con github pages

--- app/ui/Skills.jsx
+++ app/ui/Skills.jsx
@@ -1,10 +1,14 @@
+import dynamic from "next/dynamic";
 import SectionContainer from "../components/SectionContainer";
-import SkillsCanvas from "../components/skills/SkillsCanvas";
 import SkillsDescription from "../components/skills/SkillsDescription";
+const SkillsCanvas = dynamic(() => import("../components/skills/SkillsCanvas"));
 
 function Skills() {
   return (
-    <SectionContainer mdCols={"md:col-start-1 md:col-end-3"} mdRows={"md:row-start-1 md:row-end-6"}>
+    <SectionContainer
+      mdCols={"md:col-start-1 md:col-end-3"}
+      mdRows={"md:row-start-1 md:row-end-6"}
+    >
       <div id="skills">
         <SkillsDescription />
         <SkillsCanvas />
--- i18n.js
+++ i18n.js
@@ -1,14 +1,17 @@
-import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
- 
-// Can be imported from a shared config
-const locales = ['en', 'es'];
- 
-export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
+import { notFound } from "next/navigation";
+import { getRequestConfig, unstable_setRequestLocale } from "next-intl/server";
+
+// Puede ser importado desde una configuración compartida
+const locales = ["en", "es"];
+
+export default getRequestConfig(async ({ locale }) => {
+  // Valida que el parámetro `locale` entrante sea válido
   if (!locales.includes(locale)) notFound();
- 
+
+  // Configura el idioma para el renderizado estático
+  unstable_setRequestLocale(locale);
+
   return {
-    messages: (await import(`./locales/${locale}.json`)).default
+    messages: (await import(`./locales/${locale}.json`)).default,
   };
 });
